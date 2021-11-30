89d8e09773ce2193741d46185c19d3da3eb5454a
rishi dubey <rishi18454@gmail.com>
2021-11-30 20:20:50 +0530

    bst traverse level order added

--- bst.cpp
+++ bst.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <limits.h>
+#include <queue>
 using namespace std;
 
 class BstNode;
@@ -11,6 +12,8 @@ int findMin(BstNode *root);
 int getHeight(BstNode *root);
 void printInorder(BstNode *root);
 void printPreorder(BstNode *root);
+void printLevelOrder(BstNode *root);
+void printLevHelper(BstNode *root, std::queue<BstNode *> q);
 
 class BstNode
 {
@@ -30,6 +33,8 @@ public:
     friend void printInorder(BstNode *root);
     friend void printPreorder(BstNode *root);
     friend void printPostorder(BstNode *root);
+    friend void printLevelOrder(BstNode *root);
+    friend void printLevHelper(BstNode *root, std::queue<BstNode *> q);
 
     int getData()
     {
@@ -143,17 +148,46 @@ void printPostorder(BstNode *root)
     std::cout << root->data << " ";
 }
 
+void printLevHelper(BstNode *root, std::queue<BstNode *> q)
+{
+    if (root == nullptr)
+        return;
+
+    if (q.empty())
+        q.push(root);
+
+    // q contains addresss of bst nodes, first print the front, then if it has left and right children, push them to end of queue, then pop front as this element is done, then move to the next frontmost element of queue
+    cout << q.front()->data << " ";
+
+    if (q.front()->left != nullptr)
+        q.push(q.front()->left);
+    if (q.front()->right != nullptr)
+        q.push(q.front()->right);
+
+    q.pop();
+    if (!q.empty())
+        printLevHelper(q.front(), q);
+
+    return;
+}
+
+void printLevelOrder(BstNode *root)
+{
+    std::queue<BstNode *> q{};
+    printLevHelper(root, q);
+}
+
 int main()
 {
     // initialize an empty binary tree
     BstNode *root = nullptr;
 
-    // insertion
-    insert(&root, 5);
-    insert(&root, 10);
-    insert(&root, 1);
-    insert(&root, 7);
-    insert(&root, 1);
+    vector<int> vals{5, 10, 1, 7, 1, 3, 6, 11};
+
+    for (auto num : vals)
+    {
+        insert(&root, num);
+    }
 
     // insert(&root, 1);
     // insert(&root, 2);
@@ -161,25 +195,27 @@ int main()
     // insert(&root, 4);
     // insert(&root, 5);
 
-    cout << boolalpha << search(root, 7) << endl; // true
-    cout << search(root, 3) << endl
-         << endl; // false
+    // cout << boolalpha << search(root, 7) << endl; // true
+    // cout << search(root, 3) << endl
+    //      << endl; // false
 
-    cout << "Maximum number in tree: " << findMax(root) << endl; // 10
+    // cout << "Maximum number in tree: " << findMax(root) << endl; // 10
 
-    cout << "Minimum number in tree: " << findMin(root) << endl
-         << endl; // 1
+    // cout << "Minimum number in tree: " << findMin(root) << endl
+    //      << endl; // 1
 
-    cout << "Height of tree: " << getHeight(root) << endl
-         << endl;
+    // cout << "Height of tree: " << getHeight(root) << endl
+    //      << endl;
 
-    printInorder(root);
-    cout << endl;
-    printPreorder(root);
-    cout << endl;
-    printPostorder(root);
-    cout << endl;
-    cout << endl;
+    // printInorder(root);
+    // cout << endl;
+    // printPreorder(root);
+    // cout << endl;
+    // printPostorder(root);
+    // cout << endl;
+    // cout << endl;
+
+    printLevelOrder(root);
 
     system("pause");
     return 0;
